bf3adf73999ae179930c1790c7beb7d3e651c33c
uptobe8 <ia.uptobe@gmail.com>
2025-12-23 02:21:43 +0100

    Add trailingSlash to fix 404 errors on sector pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+    trailingSlash: true,
     output: 'export',
     basePath: '/Casos-de-uso-de-flujos-automatizados-profesionales',
  
